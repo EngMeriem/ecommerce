d435bda130c539798424bd723d446122a991f4c7
Meriem <meriem.ayed.tn@gmail.com>
2022-04-24 14:13:20 +0100

    Add bootstrap ui framework

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-Manage State By Reducer Hook
-
-1.  define reducer
-2.  update fetch data
-3.  get state from useReducer (install use-reducer-logger to show actions in console )
+- Manage State By Reducer Hook
+  1.  define reducer
+  2.  update fetch data
+  3.  get state from useReducer (install use-reducer-logger to show actions in console )
+- Add bootstrap UI Framework
+  1. npm install react-bootstrap bootstrap react-router-bootstrap
+  2. update App.js
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,5 @@
+import { Container, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen.js';
 import ProductScreen from './screens/ProductScreen.js';
@@ -5,16 +7,27 @@ import ProductScreen from './screens/ProductScreen.js';
 function App() {
   return (
     <BrowserRouter>
-      <div>
+      <div className="d-flex flex-column site-container">
         <header>
-          <Link to="/">amazona</Link>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
         </header>
         <main>
-          <Routes>
-            <Route path="/" element={<HomeScreen />} />
-            <Route path="/product/:slug" element={<ProductScreen />} />
-          </Routes>
+          <Container>
+            <Routes>
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/product/:slug" element={<ProductScreen />} />
+            </Routes>
+          </Container>
         </main>
+        <footer>
+          <div className="text-center">all rights reserved</div>
+        </footer>
       </div>
     </BrowserRouter>
   );
